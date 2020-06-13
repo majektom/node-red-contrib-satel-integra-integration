eca04c0d75377880a634507f42505aa75b10ef3a
Tomasz Maj <majektom@gmail.com>
2020-06-13 18:25:49 +0200

    Run prettier

--- encoder/encoder.test.js
+++ encoder/encoder.test.js
@@ -1,46 +1,54 @@
-const assert = require("assert")
-const catchNode = require("@node-red/nodes/core/common/25-catch")
-const encoder = require("./encoder.js")
+const assert = require("assert");
+const catchNode = require("@node-red/nodes/core/common/25-catch");
+const encoder = require("./encoder.js");
 const helper = require("node-red-node-test-helper");
-const protocol = require("satel-integra-integration-protocol")
-const sinon = require("sinon")
+const protocol = require("satel-integra-integration-protocol");
+const sinon = require("sinon");
 
-helper.init(require.resolve('node-red'));
+helper.init(require.resolve("node-red"));
 
-describe('satel-integra-encoder Node', function () {
+describe("satel-integra-encoder Node", function () {
   beforeEach(function (done) {
-      helper.startServer(done);
+    helper.startServer(done);
   });
 
   afterEach(function (done) {
-      helper.unload();
-      helper.stopServer(done);
-      sinon.restore();
+    helper.unload();
+    helper.stopServer(done);
+    sinon.restore();
   });
 
-  it('should be loaded', function () {
+  it("should be loaded", function () {
     return new Promise(function (resolve, reject) {
-      const flow = [{ id: "n1", type: "satel-integra-encoder", name: "Encoder" }];
+      const flow = [
+        { id: "n1", type: "satel-integra-encoder", name: "Encoder" },
+      ];
       helper.load(encoder, flow, function () {
         const encoderNode = helper.getNode("n1");
         try {
-          encoderNode.should.have.property('name', 'Encoder');
+          encoderNode.should.have.property("name", "Encoder");
           resolve();
-        } catch(error) {
+        } catch (error) {
           reject(error);
         }
       });
     });
   });
 
-  it('should issue error on message without topic', function () {
+  it("should issue error on message without topic", function () {
     return new Promise(function (resolve, reject) {
       const flow = [
         { id: "f1", type: "tab", label: "Test flow" },
-        { id: "n1", z: "f1", type: "satel-integra-encoder", name: "Encoder", wires: [["n2"]] },
+        {
+          id: "n1",
+          z: "f1",
+          type: "satel-integra-encoder",
+          name: "Encoder",
+          wires: [["n2"]],
+        },
         { id: "n2", z: "f1", type: "helper" },
         { id: "n3", z: "f1", type: "helper" },
-        { id: "n4", z: "f1", type: "catch", wires: [["n3"]] }
+        { id: "n4", z: "f1", type: "catch", wires: [["n3"]] },
       ];
       helper.load([encoder, catchNode], flow, function () {
         const encoderNode = helper.getNode("n1");
@@ -49,7 +57,7 @@ describe('satel-integra-encoder Node', function () {
         helperNode1.on("input", function (msg) {
           try {
             assert.fail("message should have been discarded");
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
@@ -57,10 +65,14 @@ describe('satel-integra-encoder Node', function () {
           try {
             msg.should.not.have.property("topic");
             msg.should.have.property("error");
-            msg.error.should.have.property(
-                "source", { id: 'n1', type: 'satel-integra-encoder', name: 'Encoder', count: 1});
+            msg.error.should.have.property("source", {
+              id: "n1",
+              type: "satel-integra-encoder",
+              name: "Encoder",
+              count: 1,
+            });
             resolve();
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
@@ -71,14 +83,20 @@ describe('satel-integra-encoder Node', function () {
     });
   });
 
-  it('should issue error on message with unrecognized topic', function () {
+  it("should issue error on message with unrecognized topic", function () {
     return new Promise(function (resolve, reject) {
       const flow = [
         { id: "f1", type: "tab", label: "Test flow" },
-        { id: "n1", z: "f1", type: "satel-integra-encoder", name: "Encoder", wires: [["n2"]] },
+        {
+          id: "n1",
+          z: "f1",
+          type: "satel-integra-encoder",
+          name: "Encoder",
+          wires: [["n2"]],
+        },
         { id: "n2", z: "f1", type: "helper" },
         { id: "n3", z: "f1", type: "helper" },
-        { id: "n4", z: "f1", type: "catch", wires: [["n3"]] }
+        { id: "n4", z: "f1", type: "catch", wires: [["n3"]] },
       ];
       helper.load([encoder, catchNode], flow, function () {
         const encoderNode = helper.getNode("n1");
@@ -87,7 +105,7 @@ describe('satel-integra-encoder Node', function () {
         helperNode1.on("input", function (msg) {
           try {
             assert.fail("message should have been discarded");
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
@@ -95,25 +113,34 @@ describe('satel-integra-encoder Node', function () {
           try {
             msg.should.have.property("topic", "invalid_topic");
             msg.should.have.property("error");
-            msg.error.should.have.property(
-                "source", { id: 'n1', type: 'satel-integra-encoder', name: 'Encoder', count: 1});
+            msg.error.should.have.property("source", {
+              id: "n1",
+              type: "satel-integra-encoder",
+              name: "Encoder",
+              count: 1,
+            });
             resolve();
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
-        encoderNode.receive({ "topic": "invalid_topic" });
+        encoderNode.receive({ topic: "invalid_topic" });
       });
     }).catch(function (error) {
       assert.fail(error);
     });
   });
 
-  it('should properly encode new_data command', function () {
+  it("should properly encode new_data command", function () {
     return new Promise(function (resolve, reject) {
       const flow = [
-        { id: "n1", type: "satel-integra-encoder", name: "Encoder", wires: [["n2"]] },
-        { id: "n2", type: "helper" }
+        {
+          id: "n1",
+          type: "satel-integra-encoder",
+          name: "Encoder",
+          wires: [["n2"]],
+        },
+        { id: "n2", type: "helper" },
       ];
       helper.load([encoder, catchNode], flow, function () {
         const encoderNode = helper.getNode("n1");
@@ -121,24 +148,32 @@ describe('satel-integra-encoder Node', function () {
         helperNode.on("input", function (msg) {
           try {
             msg.should.have.property("topic", "new_data");
-            msg.should.have.property("payload", new protocol.NewDataCommand().encode());
+            msg.should.have.property(
+              "payload",
+              new protocol.NewDataCommand().encode()
+            );
             resolve();
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
-        encoderNode.receive({ "topic": "new_data" });
+        encoderNode.receive({ topic: "new_data" });
       });
     }).catch(function (error) {
       assert.fail(error);
     });
   });
 
-  it('should properly encode outputs_state command', function () {
+  it("should properly encode outputs_state command", function () {
     return new Promise(function (resolve, reject) {
       const flow = [
-        { id: "n1", type: "satel-integra-encoder", name: "Encoder", wires: [["n2"]] },
-        { id: "n2", type: "helper" }
+        {
+          id: "n1",
+          type: "satel-integra-encoder",
+          name: "Encoder",
+          wires: [["n2"]],
+        },
+        { id: "n2", type: "helper" },
       ];
       helper.load([encoder, catchNode], flow, function () {
         const encoderNode = helper.getNode("n1");
@@ -146,24 +181,32 @@ describe('satel-integra-encoder Node', function () {
         helperNode.on("input", function (msg) {
           try {
             msg.should.have.property("topic", "outputs_state");
-            msg.should.have.property("payload", new protocol.OutputsStateCommand().encode());
+            msg.should.have.property(
+              "payload",
+              new protocol.OutputsStateCommand().encode()
+            );
             resolve();
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
-        encoderNode.receive({ "topic": "outputs_state" });
+        encoderNode.receive({ topic: "outputs_state" });
       });
     }).catch(function (error) {
       assert.fail(error);
     });
   });
 
-  it('should properly encode zones_tamper command', function () {
+  it("should properly encode zones_tamper command", function () {
     return new Promise(function (resolve, reject) {
       const flow = [
-        { id: "n1", type: "satel-integra-encoder", name: "Encoder", wires: [["n2"]] },
-        { id: "n2", type: "helper" }
+        {
+          id: "n1",
+          type: "satel-integra-encoder",
+          name: "Encoder",
+          wires: [["n2"]],
+        },
+        { id: "n2", type: "helper" },
       ];
       helper.load([encoder, catchNode], flow, function () {
         const encoderNode = helper.getNode("n1");
@@ -171,24 +214,32 @@ describe('satel-integra-encoder Node', function () {
         helperNode.on("input", function (msg) {
           try {
             msg.should.have.property("topic", "zones_tamper");
-            msg.should.have.property("payload", new protocol.ZonesTamperCommand().encode());
+            msg.should.have.property(
+              "payload",
+              new protocol.ZonesTamperCommand().encode()
+            );
             resolve();
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
-        encoderNode.receive({ "topic": "zones_tamper" });
+        encoderNode.receive({ topic: "zones_tamper" });
       });
     }).catch(function (error) {
       assert.fail(error);
     });
   });
 
-  it('should properly encode zones_violation command', function () {
+  it("should properly encode zones_violation command", function () {
     return new Promise(function (resolve, reject) {
       const flow = [
-        { id: "n1", type: "satel-integra-encoder", name: "Encoder", wires: [["n2"]] },
-        { id: "n2", type: "helper" }
+        {
+          id: "n1",
+          type: "satel-integra-encoder",
+          name: "Encoder",
+          wires: [["n2"]],
+        },
+        { id: "n2", type: "helper" },
       ];
       helper.load([encoder, catchNode], flow, function () {
         const encoderNode = helper.getNode("n1");
@@ -196,13 +247,16 @@ describe('satel-integra-encoder Node', function () {
         helperNode.on("input", function (msg) {
           try {
             msg.should.have.property("topic", "zones_violation");
-            msg.should.have.property("payload", new protocol.ZonesViolationCommand().encode());
+            msg.should.have.property(
+              "payload",
+              new protocol.ZonesViolationCommand().encode()
+            );
             resolve();
-          } catch(error) {
+          } catch (error) {
             reject(error);
           }
         });
-        encoderNode.receive({ "topic": "zones_violation" });
+        encoderNode.receive({ topic: "zones_violation" });
       });
     }).catch(function (error) {
       assert.fail(error);
